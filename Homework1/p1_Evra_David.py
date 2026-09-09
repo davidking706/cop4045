c0ddadcd393046d5d86562c64c662358f69c900f
David Evra <devra2023@fau.edu>
2026-09-09 19:56:04 -0400

    Add quadratic formula and solution output
    - Calculate and sort both quadratic roots
    - Handle equations with no real solutions
    - Display one or two solutions rounded to five decimal places

--- Homework1/p1_Evra_David.py
+++ Homework1/p1_Evra_David.py
@@ -1,17 +1,33 @@
 """David Evra - Homework 1, Problem 1: Quadratic Equations."""
 
-from math import pow
+from math import pow, sqrt
 
 
 def get_variable(v: str) -> float | None:
     vStr = input(f"Enter {v}: ")
     if vStr == "":
         return None
-
+    
     return float(vStr)
 
 def discriminant(a: float, b: float, c: float) -> float:
     return float(pow(b, 2) - (4 * a * c))
+
+def show_solution(a: float, b: float, d: float):
+    if d < 0:
+        print('no real solutions')
+    else:
+        x1, x2 = quadratic_formula(a, b, d)
+        if d == 0:
+            print(f'one solution: {x1:.5f}')
+        else:
+            print(f'two solutions: x1={x1:.5f} x2={x2:.5f}')
+
+def quadratic_formula(a: float, b: float, d: float) -> tuple[float, float]:
+    x_1 = (-b + sqrt(d)) / (2 * a)
+    x_2 = (-b - sqrt(d)) / (2 * a)
+
+    return tuple(sorted((x_1, x_2)))
 
 def main():
     while True:
@@ -23,7 +39,8 @@ def main():
         c = get_variable('c')
         d = discriminant(a, b, c)
 
-        print(d)
+        show_solution(a, b, d)
+
 
 
 if __name__ == "__main__":
