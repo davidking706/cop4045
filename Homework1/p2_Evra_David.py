4c4df38cfbb3184c6f4ff83647bb1283ebbcc97d
David Evra <devra2023@fau.edu>
2026-09-09 22:50:44 -0400

    Add Pythagorean number finder for Problem 2

---
+++ Homework1/p2_Evra_David.py
@@ -0,0 +1,21 @@
+"""David Evra - Homework 1, Problem 2: Pythagorean Numbers."""
+
+
+def find_Pythagorean(n: int) -> list[tuple]:
+    # Stores tuples that satisfies Pythagorean theorem.
+    possible_pythagoreans = []
+
+    # Checks all possible combination from a, b, and c from 1 through n.
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            for c in range(1, n + 1):
+                if a**2 + b**2 == c**2:
+                    possible_pythagoreans.append((a, b, c))
+
+    return possible_pythagoreans
+
+def main():
+    print(find_Pythagorean(5))
+
+if __name__ == "__main__":
+    main()
